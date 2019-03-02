831549c86fb244c6f055b280d121e4d4b0fa16fa
liyanfeng <957012567@qq.com>
2019-03-03 00:52:31 +0800

    添加blog模型,为其添加了markdown编辑器,配置django对media目录的解析

--- apps/web/admin.py
+++ apps/web/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from web.models import BlogPost
+from django.db import models
+from markdown.widgets import AdminMarkdownWidget
+
 
 # Register your models here.
+
+class BlogPostAdimin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMarkdownWidget()},
+    }
+    exclude = ['add_time']
+    prepopulated_fields = {'slug': ('title', 'author')}
+
+
+admin.site.register(BlogPost, BlogPostAdimin)
--- apps/web/models.py
+++ apps/web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    add_time = models.DateTimeField(db_index=True, auto_now_add=True)
+
+    def __str__(self):
+        return 'title: {}'.format(self.title)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'view_blog_post', None, {'slug': self.slug}
